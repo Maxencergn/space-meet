04885d1122c328b2aa6112f281a271b49747cda6
AurelBouchet <aurelien-bouchet@hotmail.fr>
2021-05-11 22:01:10 +0200

    Buttons like/dislike OK!

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -1,34 +1,51 @@
 import db from '../db';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import ProfilCard from './ProfilCard';
-import cross from '../img/cross.png'
-import heart from '../img/heart.png'
+import cross from '../img/cross.png';
+import heart from '../img/heart.png';
 
 const Match = () => {
+  const [characters, setCharacters] = useState(db.characters);
+  const [filteredCharacters, setFilteredCharacters] = useState(db.characters);
+  const [randomId, setRandomId] = useState(
+    Math.floor(Math.random() * characters.length)
+  );
 
-const [characters, setCharacters] = useState(db.characters);
-const randomId =Math.floor(Math.random() * (characters.length));
+  const HandleLike = () => {
+    let charId = filteredCharacters[randomId].characterId;
+    for(let i = 0; i < filteredCharacters.length; i++) {
+      if (filteredCharacters[i].characterId === charId) characters[charId].like = 1
+    }
+    setFilteredCharacters(characters.filter((character) => (character.like === 0)));
+    setRandomId(Math.floor(Math.random() * (filteredCharacters.length-1)));
+  };
 
-const HandleLike = () =>{
-  characters[randomId].like = true;
-}
-const HandleDislike = () => {
-  characters[randomId].like = false;
-}
+  const HandleDislike = () => {
+    let charId = filteredCharacters[randomId].characterId;
+    for(let i = 0; i < filteredCharacters.length; i++) {
+      if (filteredCharacters[i].characterId === charId) characters[charId].like = 2
+    }
+    setFilteredCharacters(characters.filter((character) => (character.like === 0)));
+    setRandomId(Math.floor(Math.random() * (filteredCharacters.length-1)));
+  };
 
-useEffect(() => {
-  setCharacters(characters.filter(character => character.like = undefined));
-}, []);
+  const HandleClick = () => {
+    console.log(characters);
+    console.log(filteredCharacters);
+  };
 
-
-console.log(characters);
   return (
     <div>
-{characters.map((character, index) => randomId === index && <ProfilCard key={index}{...character}/>)}
+      {filteredCharacters.map(
+        (character, index) =>
+          randomId === index && <ProfilCard key={index} {...character} />
+      )}
 
-<img src={heart}  alt="heart" className="heart" onClick={HandleLike}/>
-<img src={cross} alt="cross" className="cross" onClick={HandleDislike}/>
-
+      <img src={heart} alt='heart' className='heart' onClick={HandleLike} />
+      <img src={cross} alt='cross' className='cross' onClick={HandleDislike} />
+      <button type='button' onClick={HandleClick}>
+        bouton
+      </button>
     </div>
   );
 };
