a65d76e79629b4912ac4e389974f326d681cf55e
AurelBouchet <aurelien-bouchet@hotmail.fr>
2021-05-11 20:02:39 +0200

    with useEffect in Match

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -1,25 +1,33 @@
 import db from '../db';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ProfilCard from './ProfilCard';
 import cross from '../img/cross.png'
 import heart from '../img/heart.png'
+
 const Match = () => {
 
 const [characters, setCharacters] = useState(db.characters);
 const randomId =Math.floor(Math.random() * (characters.length));
 
-function HandleLike () {
-  
+const HandleLike = () =>{
+  characters[randomId].like = true;
+}
+const HandleDislike = () => {
+  characters[randomId].like = false;
 }
 
+useEffect(() => {
+  setCharacters(characters.filter(character => character.like = undefined));
+}, []);
 
-console.log(characters[0].like);
+
+console.log(characters);
   return (
     <div>
 {characters.map((character, index) => randomId === index && <ProfilCard key={index}{...character}/>)}
 
-<img src={heart}  alt="heart" className="heart" />
-<img src={cross} alt="cross" className="cross"/>
+<img src={heart}  alt="heart" className="heart" onClick={HandleLike}/>
+<img src={cross} alt="cross" className="cross" onClick={HandleDislike}/>
 
     </div>
   );
