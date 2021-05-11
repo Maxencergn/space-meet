17187794d7d04dcb2cba2300441fe1f68a461fa7
AurelBouchet <aurelien-bouchet@hotmail.fr>
2021-05-11 17:37:15 +0200

    match with buttons

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -1,23 +1,25 @@
 import db from '../db';
 import { useState } from 'react';
 import ProfilCard from './ProfilCard';
-import cross from './img/cross.png'
-import heart from './img/heart.png'
+import cross from '../img/cross.png'
+import heart from '../img/heart.png'
 const Match = () => {
 
 const [characters, setCharacters] = useState(db.characters);
 const randomId =Math.floor(Math.random() * (characters.length));
 
+function HandleLike () {
+  
+}
 
 
-
-console.log(randomId);
+console.log(characters[0].like);
   return (
     <div>
 {characters.map((character, index) => randomId === index && <ProfilCard key={index}{...character}/>)}
 
-<img src={heart} onClick={} className="heart"/>
-<img src={cross} onClick={} className="cross"/>
+<img src={heart}  alt="heart" className="heart" />
+<img src={cross} alt="cross" className="cross"/>
 
     </div>
   );
