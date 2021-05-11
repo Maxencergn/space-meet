5d0a2b42c5d594cdcd21cc46f4c29ddcfe1dd67c
Maxence <raingeonneau.m@gmail.com>
2021-05-11 15:27:58 +0200

    mise en place router

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@ import './App.css';
 // import ProfilMassEffect from './components/ProfilMassEffect';
 // import ProfilCard from './components/ProfilCard';
 import LoadingPage from './components/LoadingPage';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <LoadingPage />
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path='/'>
+              <LoadingPage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
