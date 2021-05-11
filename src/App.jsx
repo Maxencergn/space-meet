2d11a9c034e9eb5db6f20751d1e54155fd73e9e8
Maxence <raingeonneau.m@gmail.com>
2021-05-11 14:59:23 +0200

    loading-page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
-import ProfilMassEffect from './components/ProfilMassEffect';
+// import ProfilMassEffect from './components/ProfilMassEffect';
 // import ProfilCard from './components/ProfilCard';
+import LoadingPage from './components/LoadingPage';
 
 function App() {
   return (
     <div className='App'>
-      <ProfilMassEffect />
+      <LoadingPage />
     </div>
   );
 }
